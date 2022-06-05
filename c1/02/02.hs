85ad534042793dd881e1feca7a669523f5877483
kita127 <kitada.kosuke@gmail.com>
2022-06-05 22:37:51 +0900

    update

--- c1/02/02.hs
+++ c1/02/02.hs
@@ -2,8 +2,4 @@
 -- 「パトカー」＋「タクシー」の文字を先頭から交互に連結して文字列「パタトクカシーー」を得よ．
 
 main :: IO ()
-main = do
-  let a = "パトカー"
-  let b = "タクシー"
-  let res = concat $ zipWith (\a b -> a : b : "") a b
-  putStr res
+main = putStr $ concat $ zipWith (\a b -> a : b : "") "パトカー" "タクシー"
