c627d272f4e5fc65e0d6d5c59d23a9c87dbb9c74
kita127 <kitada.kosuke@gmail.com>
2022-06-12 18:18:06 +0900

    c1 06 OK

--- c1/06/06.hs
+++ c1/06/06.hs
@@ -3,20 +3,22 @@
 -- それぞれ, XとYとして求め，XとYの和集合，積集合，差集合を求めよ．
 -- さらに，’se’というbi-gramがXおよびYに含まれるかどうかを調べよ．
 
-import Data.List (group, nub, sort)
+import Data.List (group, intersect, nub, sort, union, (\\))
 
 main :: IO ()
 main = do
-  let x = genBigram "paraparaparadise"
-      y = genBigram "paragraph"
-      unions = filterUnion x y
-      products = filterProduct x y
+  let x = sort $ genBigram "paraparaparadise"
+      y = sort $ genBigram "paragraph"
+      unions = sort $ nub x `union` nub y
+      products = sort $ nub x `intersect` nub y
+      difference = sort $ nub x \\ nub y
       seInX = "se" `elem` x
       seInY = "se" `elem` y
   print x
   print y
   print unions
   print products
+  print difference
   print seInX
   print seInY
 
@@ -24,9 +26,3 @@ genBigram :: String -> [String]
 genBigram [] = []
 genBigram [_] = []
 genBigram s@(x : y : _) = (x : y : "") : genBigram (tail s)
-
-filterUnion :: [String] -> [String] -> [String]
-filterUnion xs ys = nub $ xs ++ ys
-
-filterProduct :: [String] -> [String] -> [String]
-filterProduct xs ys = map head . filter (\xs -> length xs == 2) . group . sort $ nub xs ++ nub ys
--- c1/06/06_test.sh
+++ c1/06/06_test.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 expected=`cat << DOC
-["pa","ar","ra","ap","pa","ar","ra","ap","pa","ar","ra","ad","di","is","se"]
-["pa","ar","ra","ag","gr","ra","ap","ph"]
-["pa","ar","ra","ap","ad","di","is","se","ag","gr","ph"]
+["ad","ap","ap","ar","ar","ar","di","is","pa","pa","pa","ra","ra","ra","se"]
+["ag","ap","ar","gr","pa","ph","ra","ra"]
+["ad","ag","ap","ar","di","gr","is","pa","ph","ra","se"]
 ["ap","ar","pa","ra"]
+["ad","di","is","se"]
 True
 False
 DOC`
+
 
 output=$(stack runghc 06.hs)
 
 if [ "$output" != "$expected" ]; then
     echo "NG"
+    echo "expected"
+    echo "$expected"
+    echo "output"
+    echo "$output"
     exit 1
 fi
 
 echo "OK"
+
+
