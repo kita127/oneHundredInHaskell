d9767399d6ed8131259d613260c1fe2cd9cdebb9
kita127 <kitada.kosuke@gmail.com>
2022-06-25 10:15:28 +0900

    update

--- c2/19/19.hs
+++ c2/19/19.hs
@@ -1,10 +1,18 @@
 -- 19. 各行の1コラム目の文字列の出現頻度を求め，出現頻度の高い順に並べる
 -- 各行の1列目の文字列の出現頻度を求め，その高い順に並べて表示せよ．確認にはcut, uniq, sortコマンドを用いよ．
 
+import Data.List (group, sort, sortOn)
+import Data.Ord
 import System.Environment (getArgs)
 
 main :: IO ()
 main = do
   [f] <- getArgs
   c <- readFile f
-  putStr c
+  let res = map snd . sortOn (Down . fst) . hoge . group . sort . map (head . words) . lines $ c
+  mapM_ putStrLn res
+
+-- mapM_ putStrLn res
+
+hoge :: [[a]] -> [(Int, a)]
+hoge = map (\x -> (length x, head x))
